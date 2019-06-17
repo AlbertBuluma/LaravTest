32ef14f943a5431606ec1b71ddf0c1a53daf853c
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-17 12:10:04 +0300

    Modify task factory field.

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,13 +1,10 @@
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
-
-use App\Task;
 use Faker\Generator as Faker;
 
-$factory->define(Task::class, function (Faker $faker) {
+$factory->define(App\Task::class, function (Faker $faker) {
     return [
-        'firstname'       => $faker->text(),
-        'lastname' => $faker->text()
+        'firstname' => $faker->sentence(),
+        'lastname' => $faker->sentence()
     ];
 });
