fcb11986f9ba137bc4ff16bfb68af5a2431f8df2
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-17 12:08:51 +0300

    Modify task controller.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $tasks = Task::all();
@@ -19,93 +14,47 @@ class TaskController extends Controller
         return response()->json($tasks);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
-            'title' => 'required',
-            'description' => 'required'
+            'firstname' => 'required',
+            'lastname' => 'required'
         ]);
 
         $task = Task::create($request->all());
 
         return response()->json([
-            'message' => 'Great success! New task created',
+            'message' => 'Great success! New employee created',
             'task' => $task
         ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function show(Task $task)
     {
         return $task;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Task $task)
     {
         $request->validate([
-            'firstname'       => 'nullable',
-            'lastname' => 'nullable'
+           'firstname'=> 'nullable',
+           'lastname' => 'nullable'
         ]);
 
         $task->update($request->all());
 
         return response()->json([
-            'message' => 'Great success! Task updated',
+            'message' => 'Great success! Employee updated',
             'task' => $task
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Task $task)
     {
         $task->delete();
 
         return response()->json([
-            'message' => 'Successfully deleted task!'
+            'message' => 'Successfully deleted employee!'
         ]);
     }
 }
