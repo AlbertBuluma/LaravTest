6856ef3756ea5d99db1ebc58cfd28049656c08eb
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-17 12:13:56 +0300

    Modify TaskSeeder.

--- database/seeds/TaskSeeder.php
+++ database/seeds/TaskSeeder.php
@@ -5,11 +5,6 @@ use Illuminate\Database\Seeder;
 
 class TaskSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         factory(Task::class, 10)->create();
